9eacf8a8e0647d5495bed2d2ed53eec41458e26b
Tushant Man <tushantman77@hotmail.com>
2021-06-23 16:14:55 +0545

    commit

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+const popup = document.querySelector(".popup");
+const close = document.querySelector(".close");
+window.onload = function () {
+  setTimeout(function () {
+    popup.style.display = "block";
+  }, 500);
+};
+
+close.addEventListener("click", () => {
+  popup.style.display = "none";
+});
+
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
